13fd72c0e1e9fac16a7f5dbdadd21513fec803a6
SereneAbraham2705 <84651620+SereneAbraham2705@users.noreply.github.com>
2022-10-23 22:22:52 +0530

    added high score code

--- index.js
+++ index.js
@@ -1,51 +1,105 @@
 
-var readlineSync=require('readline-sync')
-var name=readlineSync.question("Enter Your name: ");
-console.log("Welcome "+name+" to The Harry Potter quiz!")
+var readlineSync = require('readline-sync')
+const Database = require("@replit/database")
+const db = new Database();
+var highScore
+var name = readlineSync.question("Enter Your name: ");
+console.log("Welcome" + name + " to The Harry Potter quiz!")
 
-function play(q,a){
-  var useranswer=readlineSync.question(q);
-  if(useranswer==a){
-    console.log("You are right! :)") 
+async function getValue() {
+  let response = await db.get("highScore")
+  return response;
+}
+
+async function play(q, a) {
+  var useranswer = readlineSync.question(q);
+  if (useranswer == a) {
+    console.log("You are right! :)")
     score++;
   }
-  else{
+  else {
     console.log("You are wrong! :(")
   }
-  console.log("Score is:"+score);
+  console.log("Score is:" + score);
   console.log("............")
 }
 
-var question=[
+
+var question = [
   {
-  q:`
+    q: `
 	Which school did Harry go to?
 	a: Andrews
 	b: Howard
 	c: None\n`,
-  a:"b"
+    a: "b"
   },
   {
-  q:`
+    q: `
 	What is the name of Harry's owl?
 	a: Harriot
 	b: Jack
 	c: Hedwig\n`,
-  a:"c"
+    a: "c"
   },
   {
-  q:`
-	Who id Draco Malcof's dad?
+    q: `
+	Who is Draco Malcof's dad?
 	a: Lucius
 	b: Ron
 	c: Voldermort\n`,
-  a:"a"
+    a: "a"
+  },
+  {
+    q: `
+	Who is the headmaster of Hogwarts?
+	a: Lucius
+	b: Albus Dumbledore
+	c: Voldermort\n`,
+    a: "b"
+  },
+  {
+    q: `
+	Which Hogwarts house did Harry potter belong to?
+	a: Ravenlaw
+	b: Hufflepuff
+	c: Gryffindor\n`,
+    a: "c"
   }
 ]
 
-var score=0;
-for(var i=0;i<question.length;i++){
-  play(question[i].q,question[i].a)
+
+var score = 0;
+for (var i = 0; i < question.length; i++) {
+  play(question[i].q, question[i].a)
 }
-console.log("Total score is:"+score);
-console.log("Yay! You have completed the quiz.Thank you");
+
+validateAndStoreHighScore(score);
+
+async function validateAndStoreHighScore(userscore) {
+  try {
+    await getValue().then((value) => { highScore = value; })
+    if (!highScore) {
+      highScore = userscore;
+      await db.set("highScore", userscore)
+    }
+    if (userscore < highScore) {
+      console.log("Yay! You have completed the quiz.Thank you");
+      console.log("Your total score is:" + userscore);
+      console.log("HighScore is :", highScore);
+      console.log("Try beating the highscore next time")
+    }
+    else if (userscore == highScore) {
+      console.log("Yay! You have completed the quiz. Thank you");
+      console.log("Your total score is:" + userscore);
+      console.log("HighScore is :", highScore);
+      console.log("You mached the high score")
+    } else if (userscore > highScore) {
+      console.log("Yay! You have completed the quiz. Thank you");
+      console.log("You are a true potter head. You have set a new high score :", userscore)
+      db.set("highScore", userscore)
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
